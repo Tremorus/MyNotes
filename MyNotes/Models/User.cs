60ccdd6891195620e73fc1c81c593dc619c32a6e
Andrey Milovanov <andrey.milovanov@homecredit.ru>
2018-05-15 14:48:27 +0300

    Initial

---
+++ MyNotes/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MyNotes.Models
+{
+    public class User : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Login { get; set; }
+        
+
+
+    }
+}
--- MyNotes/Startup.cs
+++ MyNotes/Startup.cs
@@ -1,34 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MyNotes.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+
+// help from: https://metanit.com/sharp/aspnet5/16.2.php
 
 namespace MyNotes
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+        public IConfiguration Configuration { get; }
 
-            app.Run(async (context) =>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //В методе ConfigureServices() добавляются сервисы для Entity Framework Core:
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //Далее добавляются специфичные для Identity сервисы:
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationContext>();
+
+            /*
+             * Метод AddIdentity() позволяет установить некоторую начальную конфигурацию. 
+             * Здесь мы указываем тип пользователя и тип роли, которые будут использоваться системой Identity. 
+             * В качестве типа пользователя выступает созданный нами выше класс User, а в качестве типа роли взят стандартный класс IdentityRole, 
+             * который находится в пространстве имен Microsoft.AspNetCore.Identity.EntityFrameworkCore.
+                        
+            Метод AddEntityFrameworkStores() устанавливает тип хранилища, которое будет применяться в Identity для хранения данных. 
+            В качестве типа хранилища здесь указывается класс контекста данных.
+            */
+
+            services.AddMvc();
+            
+        }
+
+        /*
+         Затем, чтобы использовать Identity, в методе Configure() устанавливается компонент middeware - UseAuthentication. 
+         Причем это middleware вызывается перед UseMvc(), тем самым гарантируя, что ко времени обращения к системе маршрутизации,
+         контроллерам и их методам, куки должным образом обработаны и установлены.
+             */
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
+
         }
     }
 }
