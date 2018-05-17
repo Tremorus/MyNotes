38172ec0244e3162f524e21f5a0c275f23c2428f
Андрей Милованов <tremoridze@gmail.com>
2018-05-17 14:16:34 +0300

    added UsersController

---
+++ MyNotes/ViewModels/CreateUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyNotes.ViewModels
+{
+    public class CreateUserViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ MyNotes/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyNotes.ViewModels
+{
+    public class EditUserViewModel
+    {
+        public string Email { get; set; }
+        public string Id { get; set; }
+
+    }
+}
